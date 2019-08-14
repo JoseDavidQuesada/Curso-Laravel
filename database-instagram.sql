d3587a8039b39f96e7fa3540218fafc0fbd88cc7
Jose David Quesada <45718314+JoseDavidQuesada@users.noreply.github.com>
2019-08-14 08:21:36 -0600

    Update Database sin datos

--- database-instagram.sql
+++ database-instagram.sql
@@ -1,7 +1,7 @@
 -- phpMyAdmin SQL Dump
 -- version 4.8.4
 -- https://www.phpmyadmin.net/
---
+
 -- Servidor: 127.0.0.1:3306
 -- Tiempo de generación: 30-06-2019 a las 09:35:44
 -- Versión del servidor: 5.7.24
@@ -18,14 +18,9 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--
 
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `comments`
---
+
 
 DROP TABLE IF EXISTS `comments`;
 CREATE TABLE IF NOT EXISTS `comments` (
@@ -41,9 +36,9 @@ CREATE TABLE IF NOT EXISTS `comments` (
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 
---
+
 -- Estructura de tabla para la tabla `images`
---
+
 
 DROP TABLE IF EXISTS `images`;
 CREATE TABLE IF NOT EXISTS `images` (
@@ -57,10 +52,9 @@ CREATE TABLE IF NOT EXISTS `images` (
   KEY `fk_images_users` (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
--
---
+
 -- Estructura de tabla para la tabla `likes`
---
+
 
 DROP TABLE IF EXISTS `likes`;
 CREATE TABLE IF NOT EXISTS `likes` (
@@ -74,11 +68,10 @@ CREATE TABLE IF NOT EXISTS `likes` (
   KEY `fk_likes_images` (`image_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
 
---
 
---
+
+
 -- Estructura de tabla para la tabla `users`
---
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
@@ -96,27 +89,26 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
---
---
--- Restricciones para tablas volcadas
---
 
---
+-- Restricciones para tablas volcadas
+
+
+
 -- Filtros para la tabla `comments`
---
+
 ALTER TABLE `comments`
   ADD CONSTRAINT `fk_comments_images` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`),
   ADD CONSTRAINT `fk_comments_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
---
+
 -- Filtros para la tabla `images`
---
+
 ALTER TABLE `images`
   ADD CONSTRAINT `fk_images_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
---
+
 -- Filtros para la tabla `likes`
---
+
 ALTER TABLE `likes`
   ADD CONSTRAINT `fk_likes_images` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`),
   ADD CONSTRAINT `fk_likes_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
